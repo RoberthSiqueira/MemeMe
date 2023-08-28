efcb405b75037a77f4a7cee7c78cf4af444c2c7f
Roberth Siqueira <roberth.siqueira@gmail.com>
2023-08-28 20:02:42 -0300

    feat: add initial UI with a tabbar

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -2,9 +2,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: UI
+
+    private lazy var pickBarButton: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(title: "Pick", image: nil, target: self, action: #selector(pickAnImage))
+        return barButtonItem
+    }()
+
+    private lazy var toolBar: UIToolbar = {
+        let toolBar = UIToolbar(frame: .zero)
+        toolBar.translatesAutoresizingMaskIntoConstraints = false
+        toolBar.setItems([pickBarButton], animated: true)
+        return toolBar
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        addViewHierarchy()
+    }
+
+    // MARK: View
+
+    private func addViewHierarchy() {
+        view.addSubview(toolBar)
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+            toolBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            toolBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            toolBar.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+
     }
 }
 
