b5bfbff74b05ae6842ed99d2212b0b8163cb107e
Roberth Siqueira <roberth.siqueira@gmail.com>
2023-09-25 22:04:46 -0300

    feat: Add TabBarViewController

---
+++ MemeMe/Controllers/TabBarViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
+
+    // MARK: UI
+
+
+    // MARK: LIFE CYCLE METHODS
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViewControllers()
+    }
+
+    // MARK: PRIVATE METHODS
+
+    private func setupViewControllers() {
+    }
+}
